164c07514aa15aa73448a9e1bb6b1e26f09f67f5
sasmita <sasmitakd@gmail.com>
2026-07-19 00:38:08 +0530

    FirstCommitSpringbootHelloworld

---
+++ src/main/java/com/example/MySpringBootApp/MySpringBootAppApplication.java
@@ -0,0 +1,19 @@
+package com.example.MySpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MySpringBootAppApplication {
+	@GetMapping("/hello")
+	public String getResult(){
+		return ("Hello world");
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAppApplication.class, args);
+	}
+
+}
