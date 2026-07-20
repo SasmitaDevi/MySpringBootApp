ccdbac7cd2d1d1d847882d27e1d0b2e0adbc908a
sasmita <sasmitakd@gmail.com>
2026-07-20 21:00:39 +0530

    helm deployment

--- src/main/java/com/example/MySpringBootApp/MySpringBootAppApplication.java
+++ src/main/java/com/example/MySpringBootApp/MySpringBootAppApplication.java
@@ -3,10 +3,12 @@ package com.example.MySpringBootApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class MySpringBootAppApplication {
 	@GetMapping("/hello")
 	public String getResult(){
